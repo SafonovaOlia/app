9190601606c62d0c7087580d7f1ab89a456cd206
Safonova Olia <safonovaolia@gmail.com>
2022-10-20 17:06:32 +0300

    Версия_1

--- Example/task08/Program.cs
+++ Example/task08/Program.cs
@@ -6,7 +6,21 @@ namespace task08
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine ("Введите число N: ");
+            int N = Convert.ToInt32(Console.ReadLine ());
+            int i = 1;
+            while( i < N)
+            {
+                if (i % 2 > 0) 
+                {
+
+                }    
+                else
+                {            
+                    Console.Write(i+", ");
+                }    
+                i=i+1;
+            }
         }
     }
 }
