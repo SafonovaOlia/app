01bde03a0c8914eb319d5d125a1c06bf52689de4
Safonova Olia <safonovaolia@gmail.com>
2022-10-20 17:05:11 +0300

    Версия_1

---
+++ Example/task02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine ("Введите два числа: ");
+            int a = Convert.ToInt32(Console.ReadLine ());
+            int b = Convert.ToInt32(Console.ReadLine ());
+            if (b > a)
+            {
+                Console.Write("Максимальное число: "+b);
+            }
+            else
+            {
+                Console.Write("Максимальное число: "+a);
+            }  
+        }
+    }
+}
+
