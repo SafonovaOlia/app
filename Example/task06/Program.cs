bce0642c3d6e18fb169b57fbe25de2e749e3d1fa
Safonova Olia <safonovaolia@gmail.com>
2022-10-20 17:06:14 +0300

    Версия_1

--- Example/task06/Program.cs
+++ Example/task06/Program.cs
@@ -6,7 +6,16 @@ namespace task06
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine ("Введите число: ");
+            int a = Convert.ToInt32(Console.ReadLine ());
+            if (a % 2 > 0) 
+            {
+                Console.Write("является ли число чётным: Нет ");
+            }    
+            else
+            {            
+                Console.Write("является ли число чётным: Да ");
+            }     
         }
     }
 }
