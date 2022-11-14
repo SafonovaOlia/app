d2b9113a64fd27402d4539656bca371332c9b395
Safonova Olia <safonovaolia@gmail.com>
2022-11-14 17:59:25 +0300

    версия1

---
+++ Example/task29/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длинну массива: ");
+            int lenMas = Convert.ToInt32(Console.ReadLine());
+
+            int[] RanArray = new int[lenMas];
+            for (int i = 0; i < RanArray.Length; i++)
+            {
+                RanArray[i] = new Random().Next(1,9);
+                Console.Write(RanArray[i] + " ");
+            }
+        }
+    }
+}
