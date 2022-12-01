89f375a3121c38bb0ec35272d12c1ae0800bc52a
Safonova Olia <safonovaolia@gmail.com>
2022-12-01 20:20:10 +0300

    версия1

---
+++ Example/task64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Функции
+
+int NaturalNum(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNum(n, m + 1)}, ");
+    return m;
+}
+
+// текст программы
+Console.WriteLine("введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 1;
+
+if (n < 1)
+{
+    Console.WriteLine("Введите положительное число");
+}
+Console.Write($"Все натуральные числа в промежутке от {n} до {m}:->  "); 
+Console.Write($"{NaturalNum(n, m)}");   
