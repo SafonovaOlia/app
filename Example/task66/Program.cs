82b5ff061ec4d4d04978eb7b3c53420fdc9a1fa2
Safonova Olia <safonovaolia@gmail.com>
2022-12-01 20:20:24 +0300

    версия1

---
+++ Example/task66/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма натуральных элементов в промежутке от {m} до {n} = {CountNaturalSum(m, n)}");
+
+int CountNaturalSum(int m, int n)
+{
+    if (m == n)
+        return n;
+    return n + CountNaturalSum(m, n - 1);
+}
