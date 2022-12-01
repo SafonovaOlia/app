f13ec85ed8563713a2190d489402a3ac86f4c885
Safonova Olia <safonovaolia@gmail.com>
2022-12-01 20:20:36 +0300

    версия1

---
+++ Example/task68/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите M:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
